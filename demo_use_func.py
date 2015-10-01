a11ee43ff5d87cbd65e27ee00342ba7d0eec53bd
lishuhuakai <lishuhuakai@gmail.com>
2015-10-01 15:43:55 +0800

    add demo_use_func.py

---
+++ demo_use_func.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#abs函数用于得到数的绝对值
+print(abs(100))
+print(abs(-20))
+#max function
+print(max(1, 3))
+
+#数据类型转换
+print(int('123'))
+print(float('12.34'))
+print(str(100))
+print(bool(1))
+print(bool(''))
+
+#调用hex函数
+n1 = 255
+n2 = 1000
+print(str(hex(n1)), ', ',str(hex(n2)))
